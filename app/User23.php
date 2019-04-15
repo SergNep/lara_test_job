073948903e4112bdd9e3181918078dc088f5faaf
Sergey N <nepokupny.sergey@gmail.com>
2019-04-15 10:57:08 +0300

    Revert "mdl User23 update"
    This reverts commit 27c72d4bb3cc36aba2b980dfbca3f96d3db17460.

--- app/User23.php
+++ app/User23.php
@@ -7,12 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class User23 extends Model
 {
     protected $table = 'user23';
-
-    /**
-     * Bad method for test git
-     */
-    public funcition badMethod()
-    {
-
-    }
 }
